597faf0cd8c839a7ccaa94935d6c4ee74c7eeaa5
beau-haldane <beaujorgensen@gmail.com>
2020-04-22 21:36:19 +1000

    Added inventory.yaml and categories.yaml to store all inventory and category hashes. Manually tested and working.

--- src/invntri.rb
+++ src/invntri.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 require 'colorize'
 require 'tty-prompt'
 
@@ -17,16 +18,9 @@ include SearchMethods
 prompt = TTY::Prompt.new(symbols: {marker: '-'})
 
 #               ! variables !
-inventory     = [   { 'name'  => 'tension rod chrome 100mm',         'sku' => 'trod-cr-100', 'cat' => 'hardware', 'sub_cat' => 'tension_rods',   'qty' => 100,   'cost' => 1.2,  'finish' => 'chrome',   'length' => '100mm',    'thread' => 'm5' },
-                    { 'name'  => 'tension rod chrome 80mm',          'sku' => 'trod-cr-80',  'cat' => 'hardware', 'sub_cat' => 'tension_rods',   'qty' => 150,   'cost' => 1,    'finish' => 'chrome',   'length' => '80mm',     'thread' => 'm5' },
-                    { 'name'  => '14inch Remo Coated Ambassador',    'sku' => '14-ctd-amb',  'cat' => 'hardware', 'sub_cat' => 'batter_heads',   'qty' => 15,    'cost' => 45,   'coating' => 'coated',  'diameter' => 14,       'brand' => 'inde drum labs' }]
+inventory   = YAML.load(File.open(File.join(File.dirname(__FILE__), 'inventory.yml')))
 
-categories    = [   
-                    { category: 'hardware',     category_attributes: [ 'finish' ],  sub_categories:     [   {tension_rods:  ['length', 'thread'] },
-                                                                                                            {strainers:     ['brand', 'type'] }     ] },
-                    { category: 'drum_heads',   category_attributes: [ 'coating' ], sub_categories:     [   {batter_heads:  ['diameter', 'brand'] },
-                                                                                                            {reso_heads:    ['diameter', 'brand'] }     ] }
-                ]
+categories  = YAML.load(File.open(File.join(File.dirname(__FILE__), 'categories.yml')))
 
 main_nav      = [   'Add Item',
                     'Edit Item',
@@ -38,7 +32,7 @@ main_nav      = [   'Add Item',
 exit = false
 until exit == true
     
-    # Keeps inventory sorted by cat, then sub_cat, then sku
+    # Sorts inventory hashes by cat, then sub_cat, then sku
     inventory =  inventory.sort_by { |a| [a['cat'], a['sub_cat'], a['sku'] ] }
 
     # clear terminal
@@ -63,7 +57,11 @@ until exit == true
     when main_nav[4]                                # Add/Edit Categories
         system 'clear'
         puts "#{main_nav[4]} feature coming soon."
+        File.open("categories.yml","w") { |file| file.write categories.to_yaml } 
     end
+
+    # Writes any changes to inventory.yaml
+    File.open("inventory.yml","w") { |file| file.write inventory.to_yaml }
     
     # back to main menu?
     puts
